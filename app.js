8c43c0086c9d6d1fbd7c677769c46f6b5d63a5ea
aliadamovich <alesya.argentina@gmail.com>
2024-04-22 19:11:10 -0300

    updated functions to server

--- app.js
+++ app.js
@@ -1,83 +1,147 @@
-let userNames = [];
-let tasks = [];
-function getUsers() {
+
+let currentId = 200;
+
+
+document.addEventListener("DOMContentLoaded", loadUsers);
+document.addEventListener("DOMContentLoaded", loadTasks);
+
+
+function loadUsers() {
 	fetch('https://jsonplaceholder.typicode.com/users')
 	.then(response => response.json())
-	.then(data => {
-		
-		data.forEach(user => {
-			userNames = [...userNames, user.name]
-		})
-		return userNames
-	})
+
 	.then(data => {
 		addUser(data)
 	})
 }
-getUsers()
-loadTasks()
 
 function addUser(users) {
 	const select = document.getElementById('user-todo');
-
-	for (let i = 0; i < users.length; i++) {
-		let newUser = new Option(users[i], i + 1);
+	users.forEach(user => {
+		let newUser = new Option(user.name, user.id);
 		select.append(newUser)
-	}
+	})
 }
 
 function loadTasks() {
 	fetch('https://jsonplaceholder.typicode.com/todos')
 		.then(response => response.json())
 		.then(data => {
-
-			data.forEach(task => {
-				tasks = [...tasks, task.title]
+			data.forEach(item => {
+				createCheckBox(item.title, item.userId, item.id)
 			})
-			return tasks
-		})
-		.then(data => {
-			addTaska(data)
 		})
 }
 
-function addTaska(value) {
+
+function createCheckBox(task, user, id) {
 	
-	for (let i = 0; i < value.length; i++) {
-		createCheckBox(value[i], i+1)
-	}
-}
-
-function createCheckBox(task, user) {
 	document.getElementById('todo-list').insertAdjacentHTML(
 		'afterbegin',
 		`
 		<li class="todo-item">
-		<input type="checkbox">${task} by ${user}
+		<input data-id=${id} type="checkbox">${task} by ${user}
 		<span class="close">&#9747</span>
 		</li>
 		`
 	);
 }
 
-// document.querySelector('button').addEventListener('click', newTodo);
-document.querySelector('form').addEventListener('submit', handleOnChange)
+document.querySelector('form').addEventListener('submit', submitTask);
+document.querySelector('#todo-list').addEventListener('click', deleteTask);
+document.querySelector('#todo-list').addEventListener('change', changeTaskStatus);
 
-function handleOnChange(e) {
+function submitTask(e) {
 	e.preventDefault();
 
-	let inputValue = document.getElementById('new-todo').value;
+	let inputValue = document.getElementById('new-todo').value.trim();
 	let select = document.getElementById('user-todo');
+	let selectedOption = select.options[select.selectedIndex]
 	if (select.value && inputValue) {
-		sendToServer();
+		sendTaskToServer(inputValue, selectedOption.innerText);
+		createCheckBox(inputValue, selectedOption.innerText, currentId);
 	} else {
 		alert('Required fields are not filled!!!');
-		
 	}
 }
 
-// function newTodo() {
-// 	let value = document.getElementById('new-todo').value;
-// 	let author = document.querySelector('');
-// 	document.querySelector('');
-// }
+function deleteTask(e) {
+	let closeCross = e.target.closest('.close');
+	
+	
+	if (closeCross) {
+		let delededId = closeCross.previousElementSibling.dataset.id;
+		closeCross.closest('.todo-item').remove();
+		deleteTaskFromServer(delededId);
+	}
+}
+
+function sendTaskToServer(task, name) {
+	currentId++;
+
+	fetch('https://jsonplaceholder.typicode.com/todos', {
+		method: 'POST',
+		body: JSON.stringify({
+			"userId": `${name}`,
+			"id": `${currentId}`,
+			"title": `${task}`,
+			"completed": false
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		},
+	})
+	.then(resp => {
+		if(resp.ok) {
+			return resp.json()
+		}
+		throw new Error('failed to add')
+	})
+	.then(console.log)
+	.catch(console.error)
+}
+
+function deleteTaskFromServer(id) {
+
+	fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+		method: 'DELETE',
+	})
+		.then(resp => {
+			if (resp.ok) {
+				return resp.json()
+			}
+			throw new Error('failed to add')
+		})
+		.then(console.log)
+		.catch(console.error)
+}
+
+function changeTaskStatus(e) {
+		let checkBox = e.target.closest('input')
+		let inputId = checkBox.dataset.id;
+		if (checkBox.checked) {
+			sendStatusToServer(inputId, true)
+		} else {
+			sendStatusToServer(inputId, false)
+		}
+}
+
+function sendStatusToServer(id, status) {
+	fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+		method: "PATCH",
+		body: JSON.stringify({
+			"completed": `${status}`
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		},
+	})
+		.then(resp => {
+			if (resp.ok) {
+				return resp.json()
+			}
+			throw new Error('failed to add')
+		})
+		.then(console.log)
+		.catch(console.error)
+}
